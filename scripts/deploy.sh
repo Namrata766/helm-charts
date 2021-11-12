6ae61ad408a091d7923ce67c8ee96fa6de0332ac
ambala <namrata.namrata.p@gmail.com>
2021-11-12 04:45:14 +0000

    hardcoded release name to test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ RELEASE_NAME="$2"
 
 kubectl get ns "$NAMESPACE" &> /dev/null || kubectl create ns "$NAMESPACE"
 
-helm upgrade --install "$RELEASE_NAME" pages -n "$NAMESPACE" --debug
+helm upgrade --install pagesapp pages -n "$NAMESPACE" --debug
 helm list
 kubectl config current-context
 sleep 30s
